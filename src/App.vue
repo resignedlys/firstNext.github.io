0940a6b99e4a7ceed7705395b8c8d73453e3dae8
resignedlys <100058063+resignedlys@users.noreply.github.com>
2025-01-20 09:54:53 +0800

    Create main.js

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <UnityViewer />
+  <ThreeModel />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import ThreeModel from './three3D/ThreeModel.vue';
+import UnityViewer from './three3D/UnityViewer.vue';
+</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
